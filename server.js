45a82982eed4f86808869ad357b6ff396f5e9ed1
andyashall <andy@ashall.co>
2017-02-09 11:26:00 +0000

    added dev dist and insert action

--- server.js
+++ server.js
@@ -41,18 +41,6 @@ function findUser(db, name, callback) {
   });
 };
 
-// function findMeetingId(db, meetingUrl) {
-//   var user = db.collection('meetings').findOne({url: meetingUrl}, function(err, doc){
-//     if (doc) {
-//       return doc;
-//       console.log(doc);
-//     } else {
-//       return err;
-//       console.log(doc);
-//     };
-//   });
-// };
-
 function createMeeting(db, title, id, callback) {
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
@@ -83,6 +71,30 @@ function createMeeting(db, title, id, callback) {
   });   
 };
 
+function createAction(db, actionText, id, meetingId, callback) {
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+    var Actions = db.collection('actions');
+    Actions.insertOne({
+      creator: id,
+      created: new Date(),
+      who: "",
+      actionText: actionText,
+      meetingId: meetingId,
+      completeBy: new Date(),
+      completed: ""
+    },
+    function(err, result) {
+        assert.equal(err, null);
+        console.log("Action Created");
+        callback(result);
+        db.close();
+      }
+    );
+  });   
+};
+
 function createUrl(title) {
     var dirty = title.replace(/-/g, "").replace(/\//g, "-").replace(/\s+/g, '-').toLowerCase(),
         symToText = dirty.replace(/&/g, "and").replace(/@/g, "at"),
@@ -95,8 +107,6 @@ return Math.floor(Math.pow(10, length-1) + Math.random() * 9 * Math.pow(10, leng
 };
 
 // API
-
-// var name, userId;
 
 app.post('/api/hello', function response(req, res) {
   res.write("Opening mlog, to log in please state your name");
@@ -167,23 +177,22 @@ app.post('/api/createmeeting', function response(req, res) {
 });
 
 app.post('/api/createaction', function response(req, res) {
-  var meetingName = req.body.meetingName;
   var id = req.body.userId;
-  var meetingId = "987654321";
+  var meetingId = req.body.meetingId;
+  var actionText = req.body.action;
 
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log("Connected correctly to server");
    
 
-  createMeeting(db, meetingName, id, function(data, err){
+  createAction(db, actionText, id, meetingId, function(data, err){
     if (!data) {
       console.log("error");
     } else {
       console.log(res);
       res.status(200).json({
-        message: "Created meeting called " + meetingName,
-        meetingId: meetingId
+        message: "Created action called " + actionText
       });
       res.end();
     };
@@ -193,35 +202,22 @@ app.post('/api/createaction', function response(req, res) {
   console.log(req.body);
 });
 
-app.post('/api/createaction', function response(req, res) {
-  var action = req.body.action;
-  res.status(200).json({
-    message: "Created action: " + action
-  });
-  res.end();
-  console.log(req.body);
-  // insertPage(req.body.title, req.body.body);
-});
-
-app.get('/api/getPosts', function(req, res) {
-  MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server");
+// app.get('/api/getPosts', function(req, res) {
+//   MongoClient.connect(url, function(err, db) {
+//     assert.equal(null, err);
+//     console.log("Connected correctly to server");
    
-    // insertDocuments(db, function() {
-    //   db.close();
-    // });
-    findDocuments(db, 'posts', function(data) {
-    if(err){
-        res.status(500).send("something went wrong");
-    }else{
-        res.send(data);
-        db.close();
-    }
+//     findDocuments(db, 'posts', function(data) {
+//     if(err){
+//         res.status(500).send("something went wrong");
+//     }else{
+//         res.send(data);
+//         db.close();
+//     }
 
-    });
-  });
-});
+//     });
+//   });
+// });
 
 // Webpack
 
